98cc1446d6716a2dd585a76440272827fd216046
ramironery <ramironery@gmail.com>
2021-05-26 20:34:08 -0300

    Payment, one-to-one association

--- src/main/java/com/ramironery/webservices/config/TestConfig.java
+++ src/main/java/com/ramironery/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ramironery.webservices.entities.Category;
 import com.ramironery.webservices.entities.Order;
 import com.ramironery.webservices.entities.OrderItem;
+import com.ramironery.webservices.entities.Payment;
 import com.ramironery.webservices.entities.Product;
 import com.ramironery.webservices.entities.User;
 import com.ramironery.webservices.entities.enums.OrderStatus;
@@ -82,5 +83,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
