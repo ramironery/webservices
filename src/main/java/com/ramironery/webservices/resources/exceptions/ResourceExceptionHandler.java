472ea6e8e35bb4d157607aa8bdcc8ece04481d6f
ramironery <ramironery@gmail.com>
2021-05-27 17:11:46 -0300

    Exception handling - delete

--- src/main/java/com/ramironery/webservices/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/ramironery/webservices/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ramironery.webservices.services.exceptions.DatabaseException;
 import com.ramironery.webservices.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,15 @@ public class ResourceExceptionHandler {
 		
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandarError> database(DatabaseException e, HttpServletRequest request) {
+		
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
